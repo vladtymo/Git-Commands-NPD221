9a57f4631563a2960d8d0c92d1e868a352b1de88
Kosta-111 <nestt@ukr.net>
2024-02-06 03:49:12 +0200

    Added merge form

--- GitCommands/MainForm.Designer.cs
+++ GitCommands/MainForm.Designer.cs
@@ -33,44 +33,45 @@
             label3 = new Label();
             button1 = new Button();
             buttonCheckout = new Button();
+            mergeBtn = new Button();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 32F, FontStyle.Bold);
-            label1.Location = new Point(135, 31);
+            label1.Location = new Point(118, 23);
             label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(461, 72);
+            label1.Size = new Size(368, 59);
             label1.TabIndex = 0;
             label1.Text = "GIT COMMANDS";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(47, 184);
+            label2.Location = new Point(41, 138);
             label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(0, 20);
+            label2.Size = new Size(0, 15);
             label2.TabIndex = 1;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(153, 112);
+            label3.Location = new Point(134, 84);
             label3.Margin = new Padding(2, 0, 2, 0);
             label3.Name = "label3";
-            label3.Size = new Size(360, 20);
+            label3.Size = new Size(289, 15);
             label3.TabIndex = 2;
             label3.Text = "You can read a command descripti by clicking button";
             // 
             // button1
             // 
-            button1.Location = new Point(41, 197);
-            button1.Margin = new Padding(2, 2, 2, 2);
+            button1.Location = new Point(36, 148);
+            button1.Margin = new Padding(2);
             button1.Name = "button1";
-            button1.Size = new Size(136, 38);
+            button1.Size = new Size(119, 28);
             button1.TabIndex = 3;
             button1.Text = "commit";
             button1.UseVisualStyleBackColor = true;
@@ -81,26 +82,38 @@
             buttonCheckout.BackColor = Color.FromArgb(243, 76, 40);
             buttonCheckout.FlatStyle = FlatStyle.Popup;
             buttonCheckout.Font = new Font("SimSun", 12.8F);
-            buttonCheckout.Location = new Point(41, 239);
+            buttonCheckout.Location = new Point(36, 179);
             buttonCheckout.Margin = new Padding(2);
             buttonCheckout.Name = "buttonCheckout";
-            buttonCheckout.Size = new Size(136, 38);
+            buttonCheckout.Size = new Size(119, 28);
             buttonCheckout.TabIndex = 4;
             buttonCheckout.Text = "checkout";
             buttonCheckout.UseVisualStyleBackColor = false;
             buttonCheckout.Click += buttonCheckout_Click;
             // 
+            // mergeBtn
+            // 
+            mergeBtn.Location = new Point(178, 148);
+            mergeBtn.Margin = new Padding(2);
+            mergeBtn.Name = "mergeBtn";
+            mergeBtn.Size = new Size(119, 28);
+            mergeBtn.TabIndex = 5;
+            mergeBtn.Text = "merge";
+            mergeBtn.UseVisualStyleBackColor = true;
+            mergeBtn.Click += mergeBtn_Click;
+            // 
             // MainForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(737, 456);
+            ClientSize = new Size(645, 342);
+            Controls.Add(mergeBtn);
             Controls.Add(buttonCheckout);
             Controls.Add(button1);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
-            Margin = new Padding(2, 2, 2, 2);
+            Margin = new Padding(2);
             Name = "MainForm";
             Text = "Form1";
             ResumeLayout(false);
@@ -114,5 +127,6 @@
         private Label label3;
         private Button button1;
         private Button buttonCheckout;
+        private Button mergeBtn;
     }
 }
